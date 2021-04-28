35e06b15c809240ef637696f0149dd6faeae37b2
LucasSardo <lucassardo@hotmail.com>
2021-04-28 09:30:54 -0300

    Update app.js

--- app.js
+++ app.js
@@ -94,6 +94,10 @@ app.post('/controllerUpd',urlencodedParser,function(req,res){
 })
 
 //INICIAR O SERVIDOR
-app.listen(process.env.PORT || 3000);
+//app.listen(process.env.PORT || 3000);
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
 
 
